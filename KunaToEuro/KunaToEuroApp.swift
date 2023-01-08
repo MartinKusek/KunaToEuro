60e22ed1bdc198f4dcc5b0aa9bbaba33c9b7f063
MartinK <martin.kusek1@gmail.com>
2023-01-08 18:02:28 +0100

    Initial Commit

---
+++ KunaToEuro/KunaToEuroApp.swift
@@ -0,0 +1,17 @@
+//
+//  KunaToEuroApp.swift
+//  KunaToEuro
+//
+//  Created by Martin Kusek on 08.01.2023..
+//
+
+import SwiftUI
+
+@main
+struct KunaToEuroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
