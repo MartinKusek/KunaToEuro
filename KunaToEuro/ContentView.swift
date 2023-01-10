175c7ecd40c348fcf872f16d7226089d392cb62c
MartinK <martin.kusek1@gmail.com>
2023-01-10 13:34:00 +0100

    Initial commit

--- KunaToEuro/ContentView.swift
+++ KunaToEuro/ContentView.swift
@@ -2,21 +2,93 @@
 //  ContentView.swift
 //  KunaToEuro
 //
-//  Created by Martin Kusek on 08.01.2023..
+//  Created by Martin Kusek on 08.01.2023.. €
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State var currency: String = "HRK"
+    var oppositeCurrency: String {
+        if currency == "HRK" {
+            return "EUR"
+        } else {
+            return "HRK"
         }
-        .padding()
     }
+    @State var value: String = ""
+    @State var convertedValue: String = "0.00"
+    
+    let currencyArray = ["HRK", "EUR"]
+    let exRate: Double = 7.53450
+    
+    private func calculate() {
+        var decimalConvertedValue = 0.0
+        if currency == "HRK" {
+            decimalConvertedValue = (Double(value) ?? 0) / exRate
+        } else {
+            decimalConvertedValue = (Double(value) ?? 0) * exRate
+        }
+        
+        convertedValue = String(format: "%.2f", decimalConvertedValue)
+    }
+
+    var body: some View {
+        
+        VStack {
+            Text("KunaToEuro")
+                .font(.largeTitle)
+                .bold()
+            
+            Picker("", selection: $currency) {
+                ForEach(currencyArray, id: \.self)
+                {
+                    Text($0)
+                }
+            }.onChange(of: currency){_ in
+                calculate()
+            }
+            .pickerStyle(.segmented)
+            .padding()
+            
+            Text("Fiksni tečaj konverzije: 1EUR = 7.53450HRK")
+                .font(.caption)
+                .padding(.top, -10)
+            
+            TextField("0.00 \(currency)", text: $value)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.decimalPad)
+                .onChange(of: value) { newValue in
+                    print(newValue)
+                    value = newValue.filter { "0123456789.".contains($0) }
+                    
+                    //If the first diget is 0 next is .
+                    if value.first == "0" {
+                        if value.count > 1 && value[value.index(value.startIndex, offsetBy: 1)] != "." {
+                            value = "0."
+                        }
+                    }
+                    //Can't start with .
+                    if value.starts(with: ".") {
+                        value.removeFirst()
+                    }
+                    //Only one .
+                    let count = value.filter { $0 == "." }.count
+                    if count > 1 {
+                        value.removeLast()
+                    }
+                    
+                    calculate()
+                }
+            
+            Text("\(convertedValue) \(oppositeCurrency)")
+                .font(.title)
+                .bold()
+                .padding()
+        }
+        .padding()        
+    }
+   
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ README.md
@@ -0,0 +1,31 @@
+
+# KunaToEuro
+
+A simple and easy-to-use currency exchange app built with SwiftUI. This app allows you to convert Croatian Kuna (HRK) to Euros and vice versa.
+
+The main screen of the app displays the current exchange rate between Euros and Croatian Kuna. To convert between the two currencies, simply enter the amount you want to convert in the input field, and the converted amount will be displayed.
+
+## Screenshots
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+
+## Features
+
+- Simple and intuitive user interface
+- Ability to convert between Euros and Croatian Kuna
+- Fixed exchange rate
+
+
+
+## Installation
+
+1. Clone the repository to your local machine
+
+```bash
+  git clone https://github.com/[username]/CurrencyExchange.git
+```
+
+2. Open the project in Xcode
+3. Build and run the app on your desired simulator or device
+    
